e7aa858642f309b88d968ce8cc7c1caec0f044e6
TrvsF <traviseff@gmail.com>
2022-10-28 11:37:34 +0100

    <3

---
+++ scripts/hearts.js
@@ -0,0 +1,25 @@
+doHeart();
+
+function doHeart() {
+    let heartlist = getHeartList();
+
+    for (let i = 0; i < heartlist.length; i++) {
+        heartlist[i].innerHTML = "<3";
+        heartlist[i].style.left = randomInt(0, 90) + "%";
+        heartlist[i].style["animation-duration"] = randomFloat(2, 10) + "s";
+        heartlist[i].style.color = "hsl(" + randomInt(2, 7) + ", " + randomInt(60, 100) + "%"  + ", " + randomInt(60, 100) + "%" + ")";
+        heartlist[i].style["font-size"] = randomFloat(1, 4) + "em";
+    }    
+}
+
+function getHeartList() {
+    return document.getElementsByClassName("gheart");
+}
+
+function randomInt(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function randomFloat(min, max) { 
+    return Math.random() * (max - min + 1) + min
+}
