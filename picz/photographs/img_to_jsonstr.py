5999b34b1e63cb7f44865c956b0ab84032e6265f
TrvsF <traviseff@gmail.com>
2025-04-25 23:21:12 +0100

    [IMAGES]

--- picz/photographs/img_to_jsonstr.py
+++ picz/photographs/img_to_jsonstr.py
@@ -1,15 +1,12 @@
 import os
 
-# Gather and sort .jpg files
 jpg_files = sorted(
     [f for f in os.listdir('.') if f.lower().endswith('.jpg')],
     key=lambda x: int(os.path.splitext(x)[0]) if os.path.splitext(x)[0].isdigit() else x
 )
 
-# Prepend path
 full_paths = [f'"photographs/{f}"' for f in jpg_files]
+js_array = "const picz_arr = [\n    " + ",\n    ".join(full_paths) + "\n];"
 
-# Join as JavaScript array
-js_array = "let picz_arr = [\n    " + ",\n    ".join(full_paths) + "\n];"
-
-print(js_array)
+with open('output.txt', 'w', encoding='utf-8') as f:
+    f.write(js_array)
